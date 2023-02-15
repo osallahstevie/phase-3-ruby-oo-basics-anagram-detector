037f8b2566f5a4750c3f4c579c3f41b6ded85f55
osallahstevie <osallahstevie@gmail.com>
2023-02-15 13:14:58 +0300

    finished anagram detector lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+     
+       words.filter do |word|
+          if(self.word.size!=word.size) 
+            false
+          else
+            if(self.word.split("").sort==word.split("").sort)
+            true
+            else 
+             false
+            end
+          end
+      end
+    end
+end
+listen = Anagram.new("listen")
+
+p listen.match(%w[enlists google inlets banana])
